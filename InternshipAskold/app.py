d9f60656efa869a1e62f4f0ee7faa044d3153610
Askold220 <127395778+Askold220@users.noreply.github.com>
2024-01-30 21:06:49 +0300

    Update app.py

--- InternshipAskold/app.py
+++ InternshipAskold/app.py
@@ -1,113 +1,66 @@
 import yfinance as yf
-import pandas as pd
 import plotly.graph_objects as go
 
-class OrderBlocksIndicatorPlotter:
-    def __init__(self, symbol: str, start_date: str, end_date: str, range_value: int = 15):
-        """
-        Инициализация объекта OrderBlocksIndicatorPlotter.
-
-        Параметры:
-        - symbol (str): Тикер акции или инструмента.
-        - start_date (str): Начальная дата для загрузки данных (в формате 'YYYY-MM-DD').
-        - end_date (str): Конечная дата для загрузки данных (в формате 'YYYY-MM-DD').
-        - range_value (int): Значение для расчета структурного минимума.
-        """
+class StockChart:
+    def __init__(self, symbol, start_date, end_date):
         self.symbol = symbol
-        self.start_date = start_date
-        self.end_date = end_date
-        self.range_value = range_value
-        self.data = None
-        self.short_boxes = []  # Список коротких (красных) блоков
-        self.long_boxes = []   # Список длинных (зеленых) блоков
+        self.data = yf.download(symbol, start=start_date, end=end_date)
+        self.fig = go.Figure()
+        self.increasing_color = 'rgba(0, 255, 255, 1)'
+        self.decreasing_color = 'rgba(255, 99, 71, 1)'
+        self.candle_height = self.data['Open'] - self.data['Close']
+        self.threshold = 0.03
+        self.height_increase = 0
+        self.add_candlestick()
+        self.add_rapid_growth_indicator()
 
-    def download_data(self):
-        """
-        Загрузка данных с Yahoo Finance в заданном временном диапазоне.
-        """
-        self.data = yf.download(self.symbol, start=self.start_date, end=pd.to_datetime('today'))
+    def add_candlestick(self):
+        self.fig.add_trace(go.Candlestick(x=self.data.index,
+                            open=self.data['Open'],
+                            high=self.data['High'],
+                            low=self.data['Low'],
+                            close=self.data['Close'],
+                            increasing_line_color=self.increasing_color,
+                            decreasing_line_color=self.decreasing_color,
+                            increasing_line_width=4,
+                            decreasing_line_width=4,
+                            showlegend=False))
 
-    def identify_boxes(self):
-        """
-        Идентификация блоков на основе структурного минимума и максимума.
-        """
-        structure_low = min(self.data['Low'][:self.range_value])
+    def add_rapid_growth_indicator(self):
+        rapid_growth_indicator = go.layout.Shape(
+            type="line",
+            x0=self.data.index[0],
+            x1=self.data.index[-1],
+            y0=0,
+            y1=0,
+            line=dict(color="rgba(0, 200, 0, 0.3)", width=45, dash='solid'),
+        )
 
-        for i in range(self.range_value, len(self.data)):
-            if self.data['Low'][i] < structure_low:
-                self.short_boxes.append((i - 1, i, self.data['Low'][i], structure_low))
-            elif self.short_boxes and self.data['High'][i] > self.short_boxes[-1][3]:
-                self.short_boxes[-1] = (self.short_boxes[-1][0], i, self.data['Low'][i], self.short_boxes[-1][3])
-            elif self.short_boxes and self.data['Close'][i] > self.short_boxes[-1][2]:
-                self.short_boxes.pop()
+        for i in range(1, len(self.data)):
+            price_change_percentage = (self.data['Close'][i] - self.data['Close'][i - 1]) / self.data['Close'][i - 1]
+            if price_change_percentage > self.threshold:
+                rapid_growth_indicator['x0'] = self.data.index[i - 1]
+                rapid_growth_indicator['x1'] = self.data.index[-1]
+                rapid_growth_indicator['y0'] = (self.data['Low'][i - 1] + self.data['High'][i - 1]) / 2
+                rapid_growth_indicator['y1'] = rapid_growth_indicator['y0'] - self.height_increase
+                self.fig.add_shape(go.layout.Shape(rapid_growth_indicator, opacity=0.7))
 
-            if self.short_boxes and self.data['Close'][i] > self.short_boxes[-1][2]:
-                self.long_boxes.append((i - 1, i, self.data['Close'][i], structure_low))
-            elif self.long_boxes and self.data['Low'][i] < self.long_boxes[-1][3]:
-                self.long_boxes[-1] = (self.long_boxes[-1][0], i, self.data['Close'][i], self.long_boxes[-1][3])
-            elif self.long_boxes and self.data['Close'][i] < self.long_boxes[-1][2]:
-                self.long_boxes.pop()
+    def update_layout(self):
+        self.fig.update_layout(title=f"{self.symbol} Stock Price with Custom Indicator",
+                          xaxis_title="Date",
+                          yaxis_title="Stock Price",
+                          xaxis_rangeslider_visible=False,
+                          paper_bgcolor='black',
+                          plot_bgcolor='black',
+                          xaxis=dict(showgrid=True, gridwidth=1, gridcolor='grey'))
 
-    def plot_indicator(self):
-        """
-        Построение графика с использованием библиотеки Plotly.
-        """
-        fig = go.Figure()
+    def show_chart(self):
+        self.fig.show()
 
-        # Свечи
-        fig.add_trace(go.Candlestick(x=self.data.index, open=self.data['Open'], high=self.data['High'],
-                                     low=self.data['Low'], close=self.data['Close'],
-                                     increasing_line_color='rgba(0, 255, 255, 1)',
-                                     decreasing_line_color='rgba(255, 99, 71, 1)',
-                                     name='Candlesticks'))
-
-        # Красные блоки
-        for box in self.short_boxes:
-            fig.add_trace(go.Candlestick(x=[self.data.index[box[0]], self.data.index[box[1]]],
-                                         open=[box[2], box[2]], high=[box[2] + 0.1, box[2] + 0.1],
-                                         low=[box[2], box[2]],
-                                         increasing_line_color='rgba(255, 99, 71, 1)',
-                                         decreasing_line_color='rgba(255, 99, 71, 1)'))
-
-        # Зеленые блоки
-        for box in self.long_boxes:
-            fig.add_trace(go.Candlestick(x=[self.data.index[box[0]], self.data.index[box[1]]],
-                                         open=[box[2], box[2]], high=[box[2] + 0.1, box[2] + 0.1],
-                                         low=[box[2], box[2]],
-                                         increasing_line_color='rgba(0, 255, 255, 1)',
-                                         decreasing_line_color='rgba(0, 255, 255, 1)'))
-
-        # Идентификация центра
-        center_y = (self.data['High'].max() + self.data['Low'].min()) / 2
-
-        # Добавление двух горизонтальных линий
-        fig.add_shape(type="line", x0=self.data.index[0], x1=self.data.index[-1],
-                      y0=center_y - 15, y1=center_y - 15,
-                      line=dict(color="blue", width=1),
-                      visible='legendonly',  # Линии видны только при наведении
-                      opacity=0.7)
-
-        fig.add_shape(type="line", x0=self.data.index[0], x1=self.data.index[-1],
-                      y0=center_y + 15, y1=center_y + 15,
-                      line=dict(color="blue", width=1),
-                      visible='legendonly',  # Линии видны только при наведении
-                      opacity=0.7)
-
-        # Настройка макета
-        fig.update_layout(title='Order Blocks Indicator', xaxis_title='Date', yaxis_title='Closing Price',
-                          template="plotly_dark", height=800, hovermode="x unified")
-
-        # Центрирование правого конца диаграммы
-        fig.update_xaxes(rangeslider_visible=False, rangebreaks=[dict(bounds=["sat", "mon"])])  # Центрирование правого конца диаграммы
-
-        fig.show()
-
-# Пример использования:
-symbol = "MSFT"
+symbol = "AAPL"
 start_date = "2023-01-01"
-end_date = "2024-01-16"
+end_date = "2024-01-28"
 
-indicator = OrderBlocksIndicatorPlotter(symbol, start_date, end_date)
-indicator.download_data()
-indicator.identify_boxes()
-indicator.plot_indicator()
+stock_chart = StockChart(symbol, start_date, end_date)
+stock_chart.update_layout()
+stock_chart.show_chart()
